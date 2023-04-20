071f86b6dbab4c92583054754434bccf7d242ff4
FlagHunter01 <tf-github@frols.com>
2023-04-20 13:50:18 +0200

    Dernières modifs de cours

---
+++ creation.sql
@@ -0,0 +1,97 @@
+DROP DATABASE IF EXISTS projet;
+CREATE DATABASE projet; 
+
+USE projet;
+
+DROP TABLE IF EXISTS personne;
+CREATE TABLE personne (
+    num_client int NOT NULL PRIMARY KEY, 
+    nom varchar(30) NOT NULL,
+    prenom varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+DROP TABLE IF EXISTS ville;
+CREATE TABLE ville (
+    code_postal int NOT NULL PRIMARY KEY,
+    nom varchar(30) NOT NULL,
+    departement varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+DROP TABLE IF EXISTS centre;
+CREATE TABLE centre (
+    identifiant int NOT NULL PRIMARY KEY,
+    type_centrale varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+DROP TABLE IF EXISTS fournisseur;
+CREATE TABLE fournisseur (
+    nom varchar(45) NOT NULL PRIMARY KEY
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+DROP TABLE IF EXISTS consomme;
+CREATE TABLE consomme (
+    id_consommateur int NOT NULL PRIMARY KEY,
+    quantite int NOT NULL,
+    num_client int NOT NULL,
+    id_centre int NOT NULL,
+    FOREIGN KEY (num_client) REFERENCES personne(num_client)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY (id_centre) REFERENCES centre(identifiant)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+DROP TABLE IF EXISTS facture;
+CREATE TABLE facture (
+    num_facture int NOT NULL PRIMARY KEY,
+    num_client int NOT NULL,
+    nom_fournisseur varchar(45) NOT NULL,
+    prix_kWh DECIMAL(2,2) NOT NULL,
+
+    FOREIGN KEY (num_client) REFERENCES personne(num_client)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY  (nom_fournisseur) REFERENCES fournisseur(nom)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+DROP TABLE IF EXISTS situe;
+CREATE TABLE situe (
+    identifiant_centre int NOT NULL,
+    code_postal int NOT NULL,
+    addresse varchar(50) NOT NULL PRIMARY KEY,
+    FOREIGN KEY (code_postal) REFERENCES ville(code_postal)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY (identifiant_centre) REFERENCES centre(identifiant)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+DROP TABLE IF EXISTS appartient;
+CREATE TABLE appartient (
+    contrat int NOT NULL PRIMARY KEY,
+    nom_fournisseur varchar(45) NOT NULL,
+    identifiant_centre int NOT NULL,
+    FOREIGN KEY (identifiant_centre) REFERENCES centre(identifiant)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY (nom_fournisseur) REFERENCES fournisseur(nom)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+DROP TABLE IF EXISTS habite;
+CREATE TABLE habite (
+    num_client int NOT NULL,
+    code_postal int NOT NULL,
+    addresse varchar(50) NOT NULL PRIMARY KEY,
+    FOREIGN KEY (num_client) REFERENCES personne(num_client)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY (code_postal) REFERENCES ville(code_postal)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
--- requettes.sql
+++ requettes.sql
@@ -1,56 +1,77 @@
 
-# nombre de Centrales par fourniseur
+-- nombre de Centrales par fourniseur
 SELECT appartient.nom_fournisseur as Nom_fournisseur, COUNT(DISTINCT identifiant_centre) as Nombre_de_centrales 
 FROM appartient 
 GROUP BY appartient.nom_fournisseur; 
 
-# Fournisseurs par département
+-- Fournisseurs par département
 SELECT DISTINCT fournisseur.nom as Nom_fournisseur, ville.departement as Departement
-FROM fournisseur INNER JOIN facture, ville INNER JOIN habite
+FROM fournisseur INNER JOIN facture ON fournisseur.nom = facture.nom_fournisseur, ville INNER JOIN habite ON habite.code_postal = ville.code_postal
 WHERE facture.num_client = habite.num_client
 order by fournisseur.nom ASC, ville.departement ASC
 
 
-# Départements dans lesquels est distribuée chaque énergie pour le fournisseur 1
+-- Départements dans lesquels est distribuée chaque énergie pour le fournisseur 1
 SELECT DISTINCT ville.departement, centre.type_centrale
-FROM ville, appartient, situe, centre
+FROM ville INNER JOIN situe ON ville.code_postal = situe.code_postal, appartient INNER JOIN centre ON centre.identifiant = appartient.identifiant_centre
 WHERE STRCMP(appartient.nom_fournisseur, 'fournisseur 1') = 0 
-AND centre.identifiant = appartient.identifiant_centre
-AND situe.identifiant_centre = appartient.identifiant_centre
-AND ville.code_postal = situe.code_postal
+AND situe.identifiant_centre = appartient.identifiant_centre;
 
-# Centres et énergie du fournisseur 1 dans le département // TODO FIXE STRCMP
+-- Centres et énergie du fournisseur 1 dans le département 10
 SELECT DISTINCT centre.identifiant as Identifiant_centre, centre.type_centrale as Type_energie
-FROM centre, situe, ville
+FROM centre INNER JOIN situe ON situe.identifiant_centre = centre.identifiant, ville, appartient
 WHERE situe.code_postal = ville.code_postal
 AND ville.departement = 10
-AND EXISTS (SELECT centre.identifiant
-            FROM centre, appartient
-            AND STRCMP(appartient.nom_fournisseur, 'fournisseur 1') = 0
-            AND  centre.identifiant = appartient.identifiant_centre)
+AND STRCMP(appartient.nom_fournisseur, 'fournisseur 1') = 0;
+
+-- Revenu total du dépaertement par fournisseur
+SELECT fournisseur.nom AS Nom_fournisseur, ville.departement as Departement, SUM(facture.prix_kWh * consomme.quantite) AS Revenu_total
+FROM ville  INNER JOIN  habite ON habite.code_postal = ville.code_postal, fournisseur, centre, facture INNER JOIN consomme ON facture.num_client = consomme.num_client
+WHERE facture.num_client = habite.num_client 
+AND fournisseur.nom = facture.nom_fournisseur
+GROUP BY ville.departement, fournisseur.nom
+ORDER BY fournisseur.nom ASC, ville.departement ASC
 
 
-# Revenu total du dépaertement // TODO: check
-SELECT ville.departement as Departement, SUM(facture.prix_kWh * consomme.quantite) AS Revenu_total
-FROM facture, ville, habite, centre, consomme
-WHERE facture.num_client = consomme.num_client
-AND facture.num_client = habite.num_client 
-AND habite.code_postal = ville.code_postal
-GROUP BY ville.departement
+-- Quantité de chaque énergie distribuée par chaque Centrale
+SELECT centre.type_centrale as type_energie, centre.identifiant, SUM(DISTINCT consomme.quantite) as Quantite 
+FROM consomme, centre 
+WHERE consomme.id_centre = centre.identifiant GROUP BY consomme.id_centre 
+ORDER BY centre.identifiant ASC 
+
+-- Quantité de chaque énergie distribuée dans chaque département et en fonction du fourniseur
+SELECT fournisseur.nom AS Nom_fournisseur, ville.departement as departement, centre.type_centrale as type_energie, SUM(DISTINCT consomme.quantite) as Quantite_consommee
+FROM consomme INNER JOIN centre ON consomme.id_centre = centre.identifiant, situe INNER JOIN ville ON situe.code_postal = ville.code_postal, fournisseur INNER JOIN facture ON fournisseur.nom = facture.nom_fournisseur 
+WHERE situe.identifiant_centre = consomme.id_centre
+AND facture.num_client = consomme.num_client
+GROUP BY ville.departement, centre.type_centrale, fournisseur.nom
+ORDER BY fournisseur.nom, ville.departement ASC, centre.type_centrale ASC;
 
 
-# Quantité de chaque énergie distribuée dans chaque département
-SELECT 
-FROM 
-WHERE
+-- Revenu pour chaque énergie dans chaque département pour chaque fournisseur
+SELECT fournisseur.nom as Nom_fournisseur, ville.departement as departement, centre.type_centrale as type_energie, SUM(DISTINCT consomme.quantite * facture.prix_kWh) as Revenu_total 
+FROM consomme INNER JOIN centre ON consomme.id_centre = centre.identifiant, facture INNER JOIN fournisseur ON fournisseur.nom = facture.nom_fournisseur, situe INNER JOIN ville ON situe.code_postal = ville.code_postal
+WHERE situe.identifiant_centre = consomme.id_centre
+AND facture.num_client = consomme.num_client
+GROUP BY fournisseur.nom, centre.type_centrale
+ORDER BY fournisseur.nom ASC, ville.departement ASC, centre.type_centrale ASC;
 
-# Revenu pour chaque énergie dans chaque département
-SELECT
-FROM 
-WHERE
+-- Revenu pour chaque énergie dans chaque département du fournisseur 1
+SELECT ville.departement as departement, centre.type_centrale as type_energie, SUM(DISTINCT consomme.quantite * facture.prix_kWh) as Revenu
+FROM consomme INNER JOIN facture ON facture.num_client = consomme.num_client, centre, situe INNER JOIN ville ON situe.code_postal = ville.code_postal
+WHERE consomme.id_centre = centre.identifiant
+AND STRCMP(facture.nom_fournisseur, 'fournisseur 1') = 0
+AND situe.identifiant_centre = consomme.id_centre
+GROUP BY ville.departement, centre.type_centrale
+ORDER BY ville.departement ASC, centre.type_centrale ASC;
 
-# Prix de la facture de chaque Client en fonction de sa consommation sur le mois
+-- Prix de la facture de chaque Client en fonction de sa consommation sur le mois
 SELECT DISTINCT facture.num_client, (facture.prix_kWh * consomme.quantite) AS Prix_total_facture
-FROM facture, ville, habite, centre, consomme
-WHERE facture.num_client = consomme.num_client
-GROUP BY facture.num_client
+FROM facture INNER JOIN consomme ON facture.num_client = consomme.num_client;
+
+-- Nombre total de personnes en fonction du département et du fournisseur
+SELECT fournisseur.nom, ville.departement as Departement, COUNT(DISTINCT personne.num_client) as Nombre_clients
+FROM personne, habite INNER JOIN ville ON habite.code_postal = ville.code_postal, fournisseur
+WHERE habite.num_client = personne.num_client
+GROUP BY fournisseur.nom, ville.departement
+ORDER BY fournisseur.nom;
